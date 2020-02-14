a056ea3de5181d2fc093765222bff1ff7d0698e8
Lester Young <lyoung070789@gmail.com>
2020-02-14 20:05:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -106,7 +106,17 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+new_array=[]
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday, items|
+      items.each do |items|
+      if items == "BBQ"
+        new_array << holiday
+        end
+      end
+    end
+  end
+  return new_array
 end
 
 
