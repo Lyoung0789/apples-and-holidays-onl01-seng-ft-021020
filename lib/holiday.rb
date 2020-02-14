a09d800eacf859d6cd998161a51dcbe4d9754daf
Lester Young <lyoung070789@gmail.com>
2020-02-14 19:54:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -96,7 +96,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |key, value|
     puts "#{key.capitalize}:".chomp
     value.each do |holiday , supplies|
-      puts "  #{holiday.to_s.split("_").map{|word| word.capitalize}.join(" "): #{supplies.join(", ")}"
+      puts "  #{holiday.to_s.split("_").map{|word| word.capitalize}.join(" ")}: #{supplies.join(", ")}"
     end
   end
 end
